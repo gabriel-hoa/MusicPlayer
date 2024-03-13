f169e5492e4cad29fbc5cc8b99095bec8ef58912
gabriel-hoa <gabrielhoaraujo100@gmail.com>
2024-03-13 09:50:34 -0300

    update

---
+++ app/src/main/java/com/example/musicplayer/MyApplication.kt
@@ -0,0 +1,19 @@
+package com.example.musicplayer
+
+import android.app.Application
+
+class MyApplication : Application() {
+
+    val musicPlayerManager : MusicPlayerManager by lazy {MusicPlayerManager()}
+
+    companion object {
+        lateinit var instance : MyApplication
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+
+    }
+
+}
