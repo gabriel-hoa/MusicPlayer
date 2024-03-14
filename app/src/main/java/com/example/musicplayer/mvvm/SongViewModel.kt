68165d0a2fc526a034eecee63c25fd4a6c411d0f
gabriel-hoa <gabrielhoaraujo100@gmail.com>
2024-03-14 00:02:40 -0300

    update

--- app/src/main/java/com/example/musicplayer/mvvm/SongViewModel.kt
+++ app/src/main/java/com/example/musicplayer/mvvm/SongViewModel.kt
@@ -1,4 +1,18 @@
 package com.example.musicplayer.mvvm
 
-class SongViewModel {
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import com.example.musicplayer.modal.Song
+
+@RequiresApi(Build.VERSION_CODES.R)
+class SongViewModel(private val repository: SongRepository) : ViewModel() {
+
+    fun showtheList(context: Context) : LiveData<List<Song>> {
+
+        return  repository.getAudioFiles(context)
+
+    }
 }
